0876e653125c0ca94a9c40e64035048a20a554e2
yvexeldev <azamabdusalomov@icloud.com>
2024-03-31 23:47:55 +0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import { RouterView } from "vue-router";
+import { Temp } from "@/components";
+</script>
+
+<template>
+  <div>
+    <Temp />
+    <!-- <RouterView /> -->
+  </div>
+</template>
---
+++ src/components/Temp.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="bg-green-600 text-center font-bold text-2xl p-4 text-white">
+      Hello world!
+    </h1>
+  </div>
+</template>
